43bdc74913dab7141e09a9207593336309daebdd
Pablo <psgh1989@gmail.com>
2020-08-17 22:29:28 +0200

    Se añade docs

---
+++ docs/precache-manifest.76acf20edc05222630a2088ba04bd90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7226c68de1b93ddf3f6c528388ed7ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a0fe3b685e8926fe14",
+    "url": "/static/css/main.3ed1b1d0.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a0fe3b685e8926fe14",
+    "url": "/static/js/main.4011d98b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
